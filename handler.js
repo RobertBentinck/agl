8d3ab90b50971a0b5b1560ff556e335d4362fa3b
RobertBentinck <zhu4ng@gmail.com>
2018-10-12 12:00:10 +0800

    set up test endpoint

--- handler.js
+++ handler.js
@@ -1,14 +1,16 @@
 'use strict';
 
-module.exports.hello = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully! Hello World!',
-      input: event,
-    }),
-  };
+module.exports.hello = function(event, context, callback) {
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+    console.log(event); // Contains incoming request data (e.g., query params, headers and more)
+
+    const response = {
+      statusCode: 200,
+      headers: {
+        "x-custom-header" : "My Header Value"
+      },
+      body: JSON.stringify({ "message": "Hello World!" })
+    };
+
+    callback(null, response);
 };
